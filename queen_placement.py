22d193eb787b8e7067350462c2d583cdd8bd074f
Machz <mcgohan.4@osu.edu>
2014-12-04 09:27:49 -0500

    Modified: 	- Changed ownership of boards to the placers; makes more sense

--- queen_placement.py
+++ queen_placement.py
@@ -3,11 +3,13 @@
 import time
 import copy
 
+from board import Board
+
 class Placer(object):
 	"""Parent abstract class of all Placer classes."""
-	def __init__(self, board, num_queens):
+	def __init__(self, board_size, num_queens):
 		"""Creates Placer with board given."""
-		self.board = board
+		self.board = Board(board_size)
 		self.num_queens = num_queens
 	def start_timer(self):
 		"""Starts a timer to time execution of Placers."""
@@ -20,8 +22,8 @@ class Placer(object):
 
 class IncrementalPlacer(Placer):
 	"""Parent abstract class to all incremental queen placer methods."""
-	def __init__(self, board, num_queens):
-		super(IncrementalPlacer, self).__init__(board, num_queens)
+	def __init__(self, board_size, num_queens):
+		super(IncrementalPlacer, self).__init__(board_size, num_queens)
 		self.boards_tried = 1
 	def place_queens(self):
 		self.start_timer()
@@ -29,19 +31,22 @@ class IncrementalPlacer(Placer):
 			if not self.place_next_queen():
 				self.boards_tried += 1
 				self.board.clear()
-				if self.boards_tried % 100 == 0:
+				if self.boards_tried % 500 == 0:
 					print "Boards tried: {}; Time: {} seconds".format(self.boards_tried, time.clock() - self.start_time)
 		self.stop_timer()
-		print "Total boards created: {}".format(self.boards_tried)
+		print "Total boards created: {}\n".format(self.boards_tried)
+		print "Final board:"
+		print self.board
+		print
 	def place_next_queen(self):
 		"""To be implemented by child classes. Returns True if a queen was able to be placed on current board. If False, queen was unable to be placed."""
 		pass
 
 class GreedyIncrementalPlacer(IncrementalPlacer):
 	"""Goes through ALL options of placing queens to find a correct solution."""
-	def __init__(self, board, num_queens):
+	def __init__(self, board_size, num_queens):
 		"""Creates self with the chess board given."""
-		super(GreedyIncrementalPlacer, self).__init__(board, num_queens)
+		super(GreedyIncrementalPlacer, self).__init__(board_size, num_queens)
 		self.past_boards = []
 	def place_next_queen(self):
 		for i in xrange(len(self.board.spots)):
@@ -66,13 +71,10 @@ class GreedyIncrementalPlacer(IncrementalPlacer):
 
 class LeftmostIncrementalPlacer(IncrementalPlacer):
 	"""Places 1 queen in each column, starting with the leftmost column. It starts at the top of each column and goes down through all of the possible states where none of the previously placed queens could attack the new one."""
-	def __init__(self, board, num_queens):
-		super(LeftmostIncrementalPlacer, self).__init__(board, num_queens)
+	def __init__(self, board_size, num_queens):
+		super(LeftmostIncrementalPlacer, self).__init__(board_size, num_queens)
 		self.cycles = 1
-	def place_queens(self):
-		queens_placed = 0
-		self.start_timer()
-		while queens_placed < self.num_queens:
-			pass
-		self.stop_timer()
-		print "Total boards created: {}".format(self.cycles)
+		self.current_cols = [0 for x in xrange(self.num_queens)] # represents the current column for each row
+	def place_next_queen(self):
+		self.board.queens_placed = self.num_queens
+		return True
--- queens.py
+++ queens.py
@@ -8,17 +8,17 @@ Example: python queens.py 5 2
 
 import sys
 
-from queen_placement import GreedyIncrementalPlacer
+from queen_placement import GreedyIncrementalPlacer, LeftmostIncrementalPlacer
 from board import Board
 
 if __name__ == '__main__':
 	try:
-		board_width = int(sys.argv[1])
+		board_size = int(sys.argv[1])
 		num_queens = int(sys.argv[2])
 	except ValueError: 
 		print "queens.py must be ran with two arguments, the board width and number of queens.\n\nExample:\npython queens.py 5 2\n^ A 5x5 board would be tested for the positions of 2 queens which could not attack each other."
 		exit(1)
-	board = Board(board_width)
-	greedy_placer = GreedyIncrementalPlacer(board, num_queens)
+	greedy_placer = GreedyIncrementalPlacer(board_size, num_queens)
+	leftmost_placer = LeftmostIncrementalPlacer(board_size, num_queens)
 	greedy_placer.place_queens()
-	print board
+	leftmost_placer.place_queens()
